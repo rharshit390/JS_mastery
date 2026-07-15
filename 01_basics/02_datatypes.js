77e0c10da4bfc8af72778477917ab34638e3a967
Harshit Raj <rharshit390@gmail.com>
2026-07-15 14:40:39 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";
+// variables and data types
+let name = "harshit"
+let age = 18
+let isLoggedin = true
+
+//number=>2 to the power of 53
+//bigint
+//string=>""
+//boolean=>true or false
+//null=>empty value
+//undefined=>value is not assigned
+//symbol=>unique identifiers for objects
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isLoggedin)
+console.log(typeof null)
+console.log(typeof undefined)
