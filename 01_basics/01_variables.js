f161880ba9ab28d986f585cdd5cc92e83d68a2e9
Harshit Raj <rharshit390@gmail.com>
2026-07-15 13:43:20 +0000

    add initial variable declarations

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId=2309099
+let accountEmail="rharshit390@gmail.com"
+var accountPassword = "harshit@6231"
+accountCity="Patna"
+
+// accountId=2309100
+// console.log("accountId:", accountId);
+console.table([ accountId, accountEmail, accountPassword, accountCity]);
