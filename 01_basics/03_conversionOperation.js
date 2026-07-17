d9f90008871e7c226d3ef883f1bec9c5ee3c0c24
Harshit Raj <rharshit390@gmail.com>
2026-07-17 09:39:04 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,59 @@
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//Operation
+let val=10
+let negVal=-val
+// console.log(negVal);
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1="Hello"
+let str2="Harshit"
+let str3=str1+str2
+//console.log(str3);
+// console.log(1+2)
+// console.log("1"+"2")
+// console.log(1+"2")
+// console.log(1+2+"2")
+// console.log(1+5+"2"+2)
+// console.log("1"+2+2)
+
+let gamePoints=100
+gamePoints++
+console.log(gamePoints);
+
+
+
+
